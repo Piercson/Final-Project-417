e5b6bf12645e759e21d03121d42fc997a7dc3eb2
Piercson <piie1996@gmail.com>
2021-12-04 22:49:12 -0800

    ida complete

--- .ipynb_checkpoints/helpers-checkpoint.py
+++ .ipynb_checkpoints/helpers-checkpoint.py
@@ -1,4 +1,23 @@
 import numpy as np
+from random import randint
+import copy
+from solvers import move, get_new_state
+# BAD STATE
+# 0,6,5,4,3,1,8,2,7
+def get_random_state(goal_state,num_swaps):
+    new_state = copy.deepcopy(goal_state)
+    loc_0 = get_location(new_state,0)
+    size = len(goal_state)
+    for i in range(num_swaps):
+        while(1):
+            new_loc = move(loc_0,randint(0, 3))
+            if new_loc[0] < 0 or new_loc[1] < 0 or new_loc[0] >= size or new_loc[1] >= size:
+                continue
+            new_state = get_new_state(loc_0, new_loc, new_state)
+            loc_0 = new_loc
+            break
+    return new_state
+
 def arr_to_matrix(arr,m_width):
     M = []
     for i in range(m_width):
